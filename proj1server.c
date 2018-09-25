84bdd18c7649cf8ede43c3948b2189f41c64cbd8
Megan Thomas <33107725+megantthomas@users.noreply.github.com>
2018-09-25 06:48:19 -0400

    Update proj1server.c

--- proj1server.c
+++ proj1server.c
@@ -8,7 +8,7 @@
 
 /**
  * @Author Megan Thomas & Cody Krueger 
- * @Date 24 SEP 2018
+ * @Date 25 SEP 2018
  * CIS 457 Data Comm
  * PRJ 1-A UDP Echo Server with Sliding Window
  * 
@@ -68,78 +68,64 @@ int main(int argc, char **argv){
 			long length;
             // from https://stackoverflow.com/questions/230062/whats-the-best-way-to-check-if-a-file-exists-in-c-cross-platform
     		if (access( fileName, F_OK ) != -1){
-            
-			//
-			char* packet;
-			packet = (char*)malloc(PACKET*sizeof(char));
-			char pacNum = '0';
-			ssize_t read;
+            	char* packet;
+				packet = (char*)malloc(PACKET*sizeof(char));
+				char pacNum = '0';
+				ssize_t read;
 
-			// file exists
-			printf("File found.\n");
-			file = fopen(fileName, "rb");
+				// file exists
+				printf("File found.\n");
+				file = fopen(fileName, "rb");
 
-			//Calculate and send the number of packets
-            fseek(file, 0L, SEEK_END);
-			int sz = ftell(file);
-			fseek(file, 0L, SEEK_SET);
-			sz = (sz/PACKET);
-			char tots[10];
-			snprintf(tots, sizeof(tots), "%d", sz);
-			int pNum = sendto(sockfd, tots, strlen(tots)+1, 0, (struct sockaddr*)&clientaddr, sizeof(clientaddr));
-			printf("%s\n", tots );
+				//Calculate and send the number of packets
+            	fseek(file, 0L, SEEK_END);
+				int sz = ftell(file);
+				fseek(file, 0L, SEEK_SET);
+				sz = (sz/PACKET);
+				char tots[10];
+				snprintf(tots, sizeof(tots), "%d", sz);
+				int pNum = sendto(sockfd, tots, strlen(tots)+1, 0, (struct sockaddr*)&clientaddr, sizeof(clientaddr));
+				
+				//Send Data
+				int currentP = 0;
+				int min = 0;
+				int max = 4;
+				int numAcks = 0;
+				char ack[10];
+				while ((read = fread(packet+1, 1, PACKET, file)) > 0) {
+					//sends only packets within the window
+					if(currentP>=min && currentP<=max){
+						*packet = pacNum;
+               			int ssent = sendto(sockfd, packet, read+1, 0, (struct sockaddr*)&clientaddr, sizeof(clientaddr));
+               			printf("Sending, size is %d\n     Bytes read: %zd\n", ssent, read);
+						//printf("Contents: %s\n", packet); //testing **
+						
+						//if we recieve an ack we move the window 
+						//if no acks are recieved then the program will not send more than 5 packets
+						//and will be stuck waiting for other packets for now. Will fix this in 
+						//part two by resending lost packets 
+						if(recvfrom(sockfd, ack, HEADER, 0, (struct sockaddr*)&clientaddr, &len)){
+							printf("ack num %d recieved\n", numAcks);
+							numAcks++;
+					 		if(!(max >= sz)){
+					 			max++;
+					 			min++;
+					 		}	
+						}
 
-    //Sliding Window-------------------------------------------------
-
-    // Window 
-    // char min_c = '0';
-    // char max_c = '4';
-
-    // int MIN = 0;
-    // int MAX = 4;
-
-    // char[10] ackarr;
-    // int i = 0;
-
-    // while (min_c < packetNum < max_c) {
-    //     //send packets TODO
-    //     //recieve acks TODO
-    //     //concatinate into array?
-    //     //progress through array looking at acks
-    //     //do we need a loof around iff to do that? or will constand send and recv take care of that?
-    //     //or would they halt the progress of this?
-    //     if (ackArr[i] == min_c) {
-    //         //adjust window bounds
-    //         MIN++;
-    //         MAX++;
-            
-    //         min_c = (MIN%10) + 48;
-    //         max_c = (MAX%10) + 48;
-
-    //         i++;
-    //     }
-    //     //for v2
-    //     //wait x seconds
-    //     //if no ack, resend min 
-    
-    // //------------------------------------------------------------
-
-			while ((read = fread(packet+1, 1, PACKET, file)) > 0) {
-				*packet = pacNum;
-                    		int ssent = sendto(sockfd, packet, read+1, 0, (struct sockaddr*)&clientaddr, sizeof(clientaddr));
-                    		printf("Sending, size is %d\n     Bytes read: %zd\n", ssent, read);
-							printf("Contents: %s\n", packet); //testing **
-							
-							
-					pacNum+=1;
-					free(packet);
-					char* packet;
-					packet = (char*)malloc(PACKET*sizeof(char));
+						//update current packet info
+						pacNum+=1;
+						free(packet);
+						char* packet;
+						packet = (char*)malloc(PACKET*sizeof(char));
+						currentP++;	
+					}				
 				}
-				free(packet);
+					free(packet);
 			}
     		else{
                 //File Does Not Exist
+                //if server recieves unexpected input it automatically assumes its a new file and throws this error
     			char err[] = "File does not exist \n";
     			printf("%s", err);
 				sendto(sockfd, err, strlen(err)+1, 0, (struct sockaddr*)&clientaddr, sizeof(clientaddr));
