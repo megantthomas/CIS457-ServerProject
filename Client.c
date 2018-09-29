50f5b9f2d825cc1c62390952245fca799868d6cb
BrinyCoast <36340390+BrinyCoast@users.noreply.github.com>
2018-09-29 14:21:32 -0400

    Add files via upload
    Added cksum pseudo code

---
+++ Client.c
@@ -0,0 +1,198 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * @Author Megan Thomas & Cody Krueger 
+ * @Date 24 SEP 2018
+ * CIS 457 Data Comm
+ * PRJ 1-A UDP Echo Client with Sliding Window
+ * 
+ **/
+
+int isValidIpAddress(char *ipAddress);
+struct packet{
+  char ident;
+  u_short cs;
+  char data[1024];
+}
+
+int main(int argc, char **argv){
+    //Create Socket
+	int sockfd = socket(AF_INET,SOCK_DGRAM,0);
+	if (sockfd<0){
+		printf("There was an ERROR(1) creating the socket\n"); //**does a n\ cause a problem here?
+		return 1; 
+	}
+
+    //Port Num
+	printf("Port Number: ");
+	char sPort[100];
+	// fgets(sPort, 100, stdin);
+	// int portNum = atoi (sPort);
+
+    // for testing --------------- **
+    int portNum = 9874;
+    printf("9874 \n");
+
+    if(portNum < 1023 || portNum > 49152){
+		printf("Try again with valid port number\n");
+		return 0; 
+	}
+
+    //IP Address
+
+	printf("IP Address: ");
+	// char addr[100];
+	// fgets(addr, 100, stdin);
+	char *pos;
+	// if ((pos=strchr(addr, '\n')) != NULL){
+    // 	*pos = '\0';
+ 	// }
+
+    // for testing --------------- **
+    char addr[] = "127.0.0.1";
+     printf("%s \n", addr);
+
+    if(!(isValidIpAddress(addr))){
+    	printf("Try again with a valid IP address\n");
+    	return 0;
+    }
+
+
+    //Socket Struct 
+	struct sockaddr_in serveraddr;
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(portNum);
+	serveraddr.sin_addr.s_addr=inet_addr(addr);
+
+    //File Request
+    unsigned int len = sizeof(serveraddr);
+	printf("File: ");
+	char fName[1000];
+	fgets(fName, 1000, stdin);
+	//char *pos;
+	if ((pos=strchr(fName, '\n')) != NULL){
+    	*pos = '\0';
+    }
+	sendto(sockfd, fName, strlen(fName)+1, 0,(struct sockaddr*)&serveraddr, len);
+	
+    //New File Creation
+	printf("New file name: ");
+	char newFName[1000];
+	fgets(newFName, 1000, stdin);
+	if ((pos=strchr(newFName, '\n')) != NULL){
+    	*pos = '\0';
+    }
+	FILE* newFile;
+    newFile = fopen(newFName, "w+");
+    printf("\n");
+    // fclose(newFile);
+
+    //Recieving Data-----------------------------------------------
+
+
+
+    //TODO add packet total or no file exists recv
+    //if size 0 OR corrupt, start again
+    //if good proceed
+
+    //Window 
+    char min_c = '0';
+    char max_c = '4';
+    int MIN = 0;
+    int MAX = 4;
+
+    int datOS = 1; //data offset for meta data added
+    int packetSize = 1024+datOS;
+	int numBytes;
+	//char fContents[10];
+    char* fContents;
+    char ident[2] = "0a";
+    fContents = (char*)malloc(packetSize*sizeof(char));
+	int count = 0;
+	while (1 < (numBytes = recvfrom(sockfd, fContents, packetSize+1, 0,(struct sockaddr*)&serveraddr, &len))) {
+		//printf("%s\n", fContents );
+
+        //error checking occurs here --------------------------------------------
+
+        //check cs bytes
+        //case 4: if packet corrupt
+        //----> discard packet
+	  /*
+	    u_short cksum(u_short *buf, int count) { 
+	    register u_long sum = 0;
+	    while (count--)
+	      {
+		sum += *buf++; 
+		     if (sum & 0xFFFF0000)
+		     {
+		//carry occurred, so wrap around 
+		       sum &= 0xFFFF; sum++;
+		     }
+	      } 
+	    return ˜(sum & 0xFFFF);
+	  }
+          */
+	  //if sent is correct save to file.
+	
+        //case 1: if duplicate, discard send ack
+
+        //case 2: no packet made it... server solves see time out
+
+        //case 3: packet out of order (or in order)
+        //store in array 
+        
+
+        //File Writing
+        //if min == array[min].ident (ie the struct ident, not recv ident) {
+            //remember, only do this when the packet is in order (ie min == current packet)
+            //min should reach all elements in a file
+            //send ack
+            //writeto file
+            //remove/clr that element from array
+            //move window
+            //move total packet num (for getting out of loop)
+        //}
+
+        if (min_c < fContents[0] < max_c) {
+
+            newFile = fopen(newFName, "a");
+            //fputs(fContents, newFile);
+            fwrite(fContents+datOS, 1, numBytes-datOS, newFile);
+            fclose(newFile);
+
+            *ident = ((fContents[0]-48)%10)+48;
+            printf("Ack sent: %s\n", ident);
+            //reply for packet recieved
+            sendto(sockfd, ident, 1+1, 0,(struct sockaddr*)&serveraddr, len);
+
+            //reset buffer
+            free(fContents);
+            char* fContents;
+            fContents = (char*)malloc(packetSize*sizeof(char));
+
+            //adjust window bounds
+            MIN++;
+            MAX++;
+
+            min_c = (MIN%10) + 48;
+            max_c = (MAX%10) + 48;
+        }
+	}
+    free(fContents);
+	close(sockfd);
+}
+
+//method to check if a string is an valid ip address 
+//source: https://stackoverflow.com/questions/791982/determine-if-a-string-is-a-valid-ipv4-address-in-c
+int isValidIpAddress(char *ipAddress)
+{
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+    return result != 0;
+}
